baa79186c5ed9c1f6f96eedee51381da3a516620
smy-xf <286584560@qq.com>
2019-12-12 19:45:28 +0800

    s1

---
+++ sprinb-boot-netty-mybatis/src/main/java/com/fm/netty/mybatis/SprinbBootNettyMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.fm.netty.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbBootNettyMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbBootNettyMybatisApplication.class, args);
+    }
+
+}
---
+++ sprinb-boot-netty-mybatis/src/test/java/com/fm/netty/mybatis/SprinbBootNettyMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.netty.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbBootNettyMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
